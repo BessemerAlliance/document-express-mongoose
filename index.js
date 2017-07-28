75176383b57320294ba4dacf562a2429fa870cd6
Jay Palat <jay.palat@gmail.com>
2017-07-28 12:38:45 -0400

    Dig for schema information.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
+var _ = require("underscore");
+
 
 
 var documentExpressMongoose = function(app, mongoose) {
@@ -6,19 +8,19 @@ var documentExpressMongoose = function(app, mongoose) {
     // Add an API endpoint to be used internally by this module
     app.get('/api-docs', function(req, res) {
         try {
-            var schemas;
+
             var routes;
-
+            routes = generateApiData(app);
+            var schemas = [];
             if (mongoose) {
-                console.log("Hi Mom");
+                console.log("Looking for schemas");
                 schemas = generateSchema(mongoose);
+                console.log("schema collected");
             }
-            routes = generateApiData(app)
-
             res.send({ routes: routes, schemas: schemas });
 
         } catch (e) {
-            res.send(400, e);
+            res.status(400).send(e)
         }
     });
 }
@@ -89,13 +91,83 @@ var getRouteMethods = function(route) {
     return methods;
 };
 
+var nestedSchemas;
 var generateSchema = function(mongoose) {
-    var nestedSchemas = [];
+    console.log("Generating schema");
+    nestedSchemas = [];
 
-    // total = nestedSchemas.push['do'];
-    // total = nestedSchemas.push['rei'];
-    return nestedSchemas;
+    // Transform models object to an array
+    var schemas = _.pairs(mongoose.modelSchemas);
+    console.log("generated pair schema");
 
-};
+    // Map each schema to a readable format
+    schemas = _.map(schemas, function(schema) {
+        var info = getSchemaInfo(schema);
+        return info;
+    });
+    console.log("generated map schema", schemas);
+
+    // Add nested schemas
+    schemas = schemas.concat(nestedSchemas);
+    console.log("generated concat schema", schemas);
+
+    return schemas;
+}
+
+function getSchemaInfo(schema) {
+    console.log("**get SchemaInfo schema", schema);
+
+    // Extract schema info for all fields of a schema
+    var paths = _.map(schema[1].paths, function(path) {
+
+        // Extract field info like type, required, enum etc.
+        var info = getFieldInfo(path);
+
+        // If field is a nested array with a custom, add it's schema to nested schemas
+        if (info && info.schema)
+            nestedSchemas.push(info.schema);
+
+        return info;
+    });
+
+    // Add virtual fields to schema info
+    _.each(schema[1].virtuals, function(virtual) {
+        if (virtual.path != "id")
+            paths.push({ name: virtual.path, type: "Unknown" });
+    });
+
+    return { name: schema[0], fields: paths };
+}
+
+
+function getFieldInfo(path) {
+
+    var field = { name: path.path, type: path.instance };
+
+    if (path.options.type) {
+        field.type = path.options.type.name;
+
+        if (path.options.type instanceof Array && !path.schema)
+            field.type = path.options.type[0].name + " []";
+    }
+
+    field.min = path.options.min;
+    field.max = path.options.max;
+
+    if (path.enumValues && path.enumValues.length > 0)
+        field.enumValues = path.enumValues;
+
+    if (path.schema) {
+        // This field is a nested array with a custom schema
+        field.type = field.name;
+        // Get schema info for the array item schema
+        field.schema = getSchemaInfo([field.name, path.schema]);
+    }
+
+    if (path.isRequired)
+        field.required = true;
+
+    return field;
+}
 
 module.exports = documentExpressMongoose;
